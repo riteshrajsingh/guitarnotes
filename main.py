3d5ccc41b5cf066bea7202e2ba806f5b9c58aecf
Ritesh Singh <riteshrajsingh@gmail.com>
2026-06-07 16:44:40 +0000

    Snapshot of live PythonAnywhere deployment files

---
+++ main.py
@@ -0,0 +1,135 @@
+from flask import Flask, render_template, request, send_from_directory
+import os
+
+app = Flask(__name__, static_url_path='/static', static_folder='static')
+
+CHROMATIC_SCALE = ['A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#',
+                   'A', 'A#', 'B', 'C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#']
+CHROMATIC_SCALE_MINOR = ['Am', 'A#m', 'Bm', 'Cm', 'C#m', 'Dm', 'D#m', 'Em', 'Fm', 'F#m', 'Gm', 'G#m',
+                         'Am', 'A#m', 'Bm', 'Cm', 'C#m', 'Dm', 'D#m', 'Em', 'Fm', 'F#m', 'Gm', 'G#m']
+CHROMATIC_SCALE_MINOR.reverse()
+CHROMATIC_SCALE.reverse()
+
+OPEN_CHORDS_MAJOR = ['A', 'C', 'D', 'E', 'G', 'F']
+OPEN_CHORDS_MINOR = ['Am', 'Dm', 'Em']
+
+def get_minor_capo_position(input_chord):
+    capo_details = {}
+    input_index = CHROMATIC_SCALE_MINOR.index(input_chord)
+    for chord in OPEN_CHORDS_MINOR:
+        capo_position = CHROMATIC_SCALE_MINOR.index(chord, input_index) - input_index
+        capo_details[capo_position] = chord
+    return capo_details
+
+def get_major_capo_position(input_chord):
+    capo_details = {}
+    input_index = CHROMATIC_SCALE.index(input_chord)
+    for chord in OPEN_CHORDS_MAJOR:
+        capo_position = CHROMATIC_SCALE.index(chord, input_index) - input_index
+        capo_details[capo_position] = chord
+    return capo_details
+
+
+def get_capo_patterns(input_chord_progression):
+    capo_dict = {}
+
+    # Populate capo_dict with mappings of input chords to their possible capo transpositions
+    for input_chord in input_chord_progression:
+        if input_chord.endswith("m"):
+            capo_positions = get_minor_capo_position(input_chord)
+        else:
+            capo_positions = get_major_capo_position(input_chord)
+        capo_dict[input_chord] = capo_positions  # Store mappings
+
+    # Find common capo positions among all chords
+    keys_list = [set(capo_dict_item.keys()) for capo_dict_item in capo_dict.values()]
+    common_keys = set.intersection(*keys_list)
+
+    if not common_keys:
+        return ["No common capo positions found."]
+
+    results = []
+
+    # Build a more informative output
+    for capo in sorted(common_keys):
+        progression_chords = [
+            f"{original} → {capo_dict[original][capo]}"
+            for original in capo_dict
+        ]
+        results.append(f"Capo on {capo}: " + "; ".join(progression_chords))
+
+    return results
+
+
+
+def get_capo_patterns_old(input_chord_progression):
+    capo_dict = {}
+    for input_chord in input_chord_progression:
+        if input_chord.endswith("m"):
+            capo_positions = get_minor_capo_position(input_chord)
+            capo_dict[input_chord] = capo_positions
+        else:
+            capo_positions = get_major_capo_position(input_chord)
+            capo_dict[input_chord] = capo_positions
+
+    keys_list = [set(capo_dict_item.keys()) for capo_dict_item in capo_dict.values()]
+    common_keys = set.intersection(*keys_list)
+
+    if not common_keys:
+        return ["No common capo positions found."]
+
+    results = []
+    print(capo_dict)
+    for capo in sorted(common_keys):
+        progression_chords = [capo_dict[progression][capo] for progression in capo_dict]
+        results.append(f"Capo on {capo}: " + ", ".join(progression_chords))
+    return results
+
+@app.route('/')
+def index():
+    return render_template('main.html')
+
+@app.route('/scales')
+def scales():
+    return render_template('scales.html')
+
+@app.route('/guitar-samples/<string:string_folder>/<path:filename>')
+def serve_guitar_sample(string_folder, filename):
+    print(string_folder)
+    samples_dir = os.path.join(os.path.dirname(__file__), 'guitar-samples')
+
+    try:
+        return send_from_directory(
+            os.path.join(samples_dir, string_folder),
+            filename
+        )
+    except FileNotFoundError:
+        return "Sample not found", 404
+
+@app.route('/ear-training')
+def ear_training():
+    return render_template('ear_training.html')
+
+@app.route('/identify_notes')
+def identify_notes():
+    return render_template('identify_notes.html')
+
+@app.route("/capo-transpose", methods=["GET", "POST"])
+def capo_transposer():
+    capo_positions = None
+    selected_chords = None
+    if request.method == "POST":
+        selected_chords = request.form.getlist("chords")
+        if not selected_chords:
+            error = "Please select at least one chord."
+            return render_template("main.html", error=error)
+        capo_positions = get_capo_patterns(selected_chords)
+    return render_template("capo-transpose.html", capo_positions=capo_positions, selected_chords=selected_chords)
+
+@app.route('/chord-library')
+def chord_library():
+    return render_template('chord-library.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True,host='0.0.0.0', port=5001)
